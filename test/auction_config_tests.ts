75bf80e3af4f22a4f9b62ef9f29a3527071308b3
Carlo Pascoli <carlo.pascoli@gmail.com>
2023-08-09 10:53:48 +0100

    Cleanup config tests

--- test/auction_config_tests.ts
+++ test/auction_config_tests.ts
@@ -3,30 +3,18 @@ import { expect } from "chai";
 
 import { deployAuctionContract, toUnits, toWei } from "./helpers";
 
-
+/**
+ * Tests for the configuration of the TokenAuction contract.
+ */
 describe("TokenAuction", function () {
-
 
     describe("Config", function () {
 
 		it("has TestToken address", async function () {
-			const { testToken } = await loadFixture(deployAuctionContract);
+			const { tokenAuction, testToken } = await loadFixture(deployAuctionContract);
 
-            expect( testToken.address ).to.be.not.undefined
-		});
-
-        it("has TokenAuction address", async function () {
-			const { tokenAuction } = await loadFixture(deployAuctionContract);
-
-            expect( tokenAuction.address ).to.be.not.undefined
-		});
-
-
-        it("TestToken supply is 1M", async function () {
-			const { testToken } = await loadFixture(deployAuctionContract);
-
-            const supply = toUnits(await testToken.totalSupply());
-            expect( supply ).to.equals( 1_000_000 )
+            expect( await tokenAuction.token() ).to.be.not.undefined
+            expect( await tokenAuction.token() ).to.equals(testToken.address)
 		});
     })
 
